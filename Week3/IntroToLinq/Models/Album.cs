905de5622caebb3228b1a493510643666ec6baf6
EmeryAtRTC <102991550+EmeryAtRTC@users.noreply.github.com>
2023-04-17 15:08:24 -0700

    Added Week 3 Starting Project

---
+++ Week3/IntroToLinq/Models/Album.cs
@@ -0,0 +1,11 @@
+﻿namespace IntroToLinq.Models
+{
+    public class Album
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
